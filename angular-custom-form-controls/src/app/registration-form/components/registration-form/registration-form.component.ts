d5763e791a75dfb85c369bb364047cf9558ae197
egor <megalomania281@gmail.com>
2022-08-09 21:53:20 +0200

    implemented form

--- angular-custom-form-controls/src/app/registration-form/components/registration-form/registration-form.component.ts
+++ angular-custom-form-controls/src/app/registration-form/components/registration-form/registration-form.component.ts
@@ -1,15 +1,70 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-registration-form',
   templateUrl: './registration-form.component.html',
-  styleUrls: ['./registration-form.component.scss']
+  styleUrls: ['./registration-form.component.scss'],
 })
 export class RegistrationFormComponent implements OnInit {
+  registrationForm: FormGroup = new FormGroup({});
+  passwordFieldType: string = 'password';
 
-  constructor() { }
+  constructor(private _fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  get nameField(): FormControl {
+    return this.registrationForm.get('name') as FormControl;
   }
 
+  get emailField(): FormControl {
+    return this.registrationForm.get('email') as FormControl;
+  }
+
+  get passwordField(): FormControl {
+    return this.registrationForm.get('password') as FormControl;
+  }
+
+  get favouriteHexCodeValue(): string {
+    return '#' + this.registrationForm.get('favouriteHexCode')?.value;
+  }
+
+  ngOnInit(): void {
+    this._initRegistrationForm();
+  }
+
+  onSubmit() {
+    if (this.registrationForm.invalid) {
+      return;
+    }
+
+    const request = {
+      ...this.registrationForm.value,
+    };
+    console.log({ request });
+  }
+
+  togglePasswordVisible() {
+    this.passwordFieldType =
+      this.passwordFieldType === 'text' ? 'password' : 'text';
+  }
+
+  private _initRegistrationForm() {
+    this.registrationForm = this._fb.group({
+      name: new FormControl('', {
+        validators: [Validators.required],
+      }),
+      email: new FormControl('', {
+        validators: [Validators.required, Validators.email],
+      }),
+      password: new FormControl('', {
+        validators: [Validators.required],
+      }),
+      favouriteHexCode: new FormControl(''),
+    });
+  }
 }
--- angular-custom-form-controls/src/app/registration-form/registration-form.module.ts
+++ angular-custom-form-controls/src/app/registration-form/registration-form.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegistrationFormComponent } from './components/registration-form/registration-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [RegistrationFormComponent],
   exports: [RegistrationFormComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+  ],
 })
 export class RegistrationFormModule {}
