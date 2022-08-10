98c6bad457d6aa8645ae81caf837dac51077d6bb
egor <megalomania281@gmail.com>
2022-08-10 11:36:37 +0200

    added tests for formElementComponent

--- angular-custom-form-controls/src/app/registration-form/components/form-element/form-element.component.spec.ts
+++ angular-custom-form-controls/src/app/registration-form/components/form-element/form-element.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormElementComponent } from './form-element.component';
+import { INPUT_TYPE } from '../../models';
 
 describe('FormElementComponent', () => {
   let component: FormElementComponent;
@@ -8,9 +9,8 @@ describe('FormElementComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FormElementComponent ]
-    })
-    .compileComponents();
+      declarations: [FormElementComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +21,26 @@ describe('FormElementComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should return string and some of input types', () => {
+    expect(component.getTypeOfInput()).toBeTruthy();
+    expect(() => typeof component.getTypeOfInput()).toBe('string');
+    component.fieldName = 'email';
+    expect(() => component.getTypeOfInput()).toBe(INPUT_TYPE.EMAIL);
+  });
+
+  it('should triggered change event', () => {
+    const spy = spyOn(component, 'changed');
+    const value = 'test';
+    const eventValue = { target: { value } } as any as Event;
+    component.onChange(eventValue);
+    expect(spy).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalledWith('test');
+  });
+
+  it("should return true if it's email formField", () => {
+    component.fieldName = 'email';
+    expect(component.isEmailField).toBeTruthy();
   });
 });
