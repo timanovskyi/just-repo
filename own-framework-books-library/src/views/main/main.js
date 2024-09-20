cd6949b02f9a040bb884b91e02d4b58ab7059d19
Egor <megalomania281@gmail.com>
2024-09-20 16:43:18 +0200

    added states; implemented on change state

--- own-framework-books-library/src/views/main/main.js
+++ own-framework-books-library/src/views/main/main.js
@@ -1,14 +1,31 @@
 import { AbstractView } from "../../common/view.js";
+import onChange from "on-change";
 
 export class MainView extends AbstractView {
-  constructor() {
+  state = {
+    list: [],
+    loading: false,
+    searchQuery: undefined,
+    offset: 0,
+  };
+
+  constructor(appState) {
     super();
+    this.appState = appState;
+    this.appState = onChange(this.appState, this.appStateHook.bind(this));
     this.setTitle("Search book");
+  }
+
+  appStateHook(path) {
+    console.log(path);
+    if (path === "favorites") {
+      console.log("");
+    }
   }
 
   render() {
     const main = document.createElement("div");
-    main.innerHTML = "Test";
+    main.innerHTML = `Count of books is ${this.appState.favorites.length}`;
     this.app.innerHTML = "";
     this.app.append(main);
   }
