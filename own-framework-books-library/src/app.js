911819250165ccfee276643db0219e205aebe075
Egor <megalomania281@gmail.com>
2024-09-20 16:01:27 +0200

    added abstract view; added mainview; implemented route by hash

--- own-framework-books-library/src/app.js
+++ own-framework-books-library/src/app.js
@@ -1,3 +1,26 @@
-"use-strict";
+import { MainView } from "./views/main/main.js";
 
-import "./app.css";
+class App {
+  routes = [
+    {
+      path: "",
+      view: MainView,
+    },
+  ];
+
+  constructor() {
+    window.addEventListener("hashchange", this.route.bind(this));
+    this.route();
+  }
+
+  route() {
+    if (this.currentView) {
+      this.currentView.destoy();
+    }
+    const view = this.routes.find((r) => r.path == location.hash).view;
+    this.currentView = new view();
+    this.currentView.render();
+  }
+}
+
+new App();
