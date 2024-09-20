6c7407c4cc6609fe18dd62cc3bf366b709eddf08
Egor <megalomania281@gmail.com>
2024-09-20 17:29:35 +0200

    implemented search comp

---
+++ own-framework-books-library/src/components/search/search.js
@@ -0,0 +1,28 @@
+import { DivComponent } from "../../common/div-component.js";
+import "./search.css";
+
+export class Search extends DivComponent {
+  constructor(state) {
+    super();
+    this.state = state;
+  }
+
+  render() {
+    this.el.classList.add("search");
+    this.el.innerHTML = `
+      <div class="search__wrapper">
+          <input 
+          type="text" 
+          name="name"
+          class="search__input"
+          value="${this.state.searchQuery ? this.state.searchQuery : ""}"
+           placeholder="Search book" />
+               <img src="/static/search.svg" alt="search">
+        </div>
+        <button>
+            <img src="/static/search-white.svg" alt="search-button">
+        </button>
+    `;
+    return this.el;
+  }
+}
--- own-framework-books-library/src/views/main/main.js
+++ own-framework-books-library/src/views/main/main.js
@@ -1,6 +1,7 @@
 import { AbstractView } from "../../common/view.js";
 import onChange from "on-change";
 import { Header } from "../../components/header/header.js";
+import { Search } from "../../components/search/search.js";
 
 export class MainView extends AbstractView {
   state = {
@@ -26,7 +27,8 @@ export class MainView extends AbstractView {
 
   render() {
     const main = document.createElement("div");
-    main.innerHTML = `Count of books is ${this.appState.favorites.length}`;
+    main.append(new Search(this.state).render());
+    // main.innerHTML = `Count of books is ${this.appState.favorites.length}`;
     this.app.innerHTML = "";
     this.app.append(main);
     this.renderHeader();
